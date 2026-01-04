b2ef242890535af2f5fee7f7f0cbfb8cace4d609
Nguyen SN <nguyensn1980@yahoo.com>
2026-01-04 14:55:52 -0600

    Cap nhat FirebaseOption moi, fix loi not support

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,14 +5,28 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -20,11 +34,30 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDC0sfg8ASLJpNYQZgMPipVbhgRvdfmMJg',
+    appId: '1:183082399206:web:90098dcb3f057daa9c9227', // nếu dùng web
+    messagingSenderId: '183082399206',
+    projectId: 'sos-battery-dfa5e',
+    authDomain: 'sos-battery.firebaseapp.com',
+    storageBucket: 'sos-battery.appspot.com',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDC0sfg8ASLJpNYQZgMPipVbhgRvdfmMJg',
     appId: '1:183082399206:android:f589f7a92cb2584a9c9227',
     messagingSenderId: '183082399206',
     projectId: 'sos-battery-dfa5e',
-    storageBucket: '', // Nếu project có Cloud Storage thì điền, không thì để rỗng ""
+    storageBucket: 'sos-battery.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyDC0sfg8ASLJpNYQZgMPipVbhgRvdfmMJg',
+    appId:
+        '1:183082399206:ios:fe7a5ccd344c46549c9227', // <<< BẮT BUỘC PHẢI LÀ iOS APP ID
+    messagingSenderId: '183082399206',
+    projectId: 'sos-battery-dfa5e',
+    storageBucket: 'sos-battery.appspot.com',
+    iosBundleId: 'com.sosbattery.app',
   );
 }
